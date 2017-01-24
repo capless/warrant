355b75abdb03af3d807be7bcdff47b135571578b
Brian Jinwright <bjinwright@qwigo.com>
2017-01-24 16:07:20 -0500

    Added two more tests

--- cognito/django/tests.py
+++ cognito/django/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate
 from django.test import TransactionTestCase
 from django.conf import settings
-
+from botocore.exceptions import ClientError
 
 class AuthTests(TransactionTestCase):
 
@@ -9,3 +9,22 @@ class AuthTests(TransactionTestCase):
         user = authenticate(username=settings.COGNITO_TEST_USERNAME,
                             password=settings.COGNITO_TEST_PASSWORD)
         self.assertIsNotNone(user)
+
+    def test_user_authentication_wrong_password(self):
+        with self.assertRaises(ClientError) as em:
+
+            user = authenticate(username=settings.COGNITO_TEST_USERNAME,
+                            password=settings.COGNITO_TEST_PASSWORD+'wrong')
+
+        self.assertEquals(str(em.exception),'An error occurred (NotAuthorizedException) '\
+                                  'when calling the AdminInitiateAuth '\
+                                  'operation: Incorrect username or password.')
+
+    def test_user_authentication_wrong_username(self):
+        with self.assertRaises(ClientError) as em:
+            user = authenticate(username=settings.COGNITO_TEST_USERNAME + 'wrong',
+                                password=settings.COGNITO_TEST_PASSWORD )
+
+        self.assertEquals(str(em.exception), 'An error occurred (UserNotFoundException) '\
+                                             'when calling the AdminInitiateAuth '\
+                                             'operation: User does not exist.')
