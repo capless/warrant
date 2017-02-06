3fdf70371d6eaca4a6a716f926bb2638b9e62079
Eric Petway <eric.petway@gmail.com>
2017-02-06 09:25:52 -0500

    Added initial setup.py, moved manage.py into cdu dir.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from itertools import chain
+
+from setuptools import find_packages, setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+packages = find_packages(exclude=['tests*'])
+
+setup(
+    name='cognito',
+    version='0.1',
+    packages=packages,
+    include_package_data=True,
+    description='Cognito integration using boto3.',
+    long_description=README,
+    author='MetaMetrics, Inc.',
+    author_email='engineering@lexile.com',
+    url='http://www.metametricsinc.com/',
+    license='GPLv3',
+    install_requires=[
+        'boto3>=1.4,<1.5',
+    ],
+    extras_require={
+        'django': ['Django>=1.10,<1.11'],
+    }
+)
