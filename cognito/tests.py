825dafbe80f73fb88e443d58886b603c8f823894
Brian Jinwright <bjinwright@qwigo.com>
2017-02-14 11:03:45 -0500

    Stubbed out test

--- cognito/tests.py
+++ cognito/tests.py
@@ -7,12 +7,54 @@ from cognito import User,UserObj
 class ConitoTestCase(unittest.TestCase):
 
     def setUp(self):
-        username = env('COGNITO_TEST_USERNAME')
-        password = env('COGNITO_TEST_PASSWORD')
-        self.user_a = User(username=username,password=password)
+        self.username = env('COGNITO_TEST_USERNAME')
+        self.password = env('COGNITO_TEST_PASSWORD')
+        self.user_a = User(username=self.username,
+                           password=self.password)
         self.user_a.authenticate()
-        self.user_b = User(id_token=self.user_a.id_token)
+        self.user_b = User(id_token=self.user_a.id_token,
+                           access_token=self.user_a.access_token,
+                           refresh_token=self.user_b.refresh_token)
 
     def test_authenticate(self):
+        pass
+
+    def test_authenticate_wrong_password(self):
+        pass
+
+    def test_authenticate_wrong_username(self):
+        pass
+
+    def test_register(self):
+        pass
+
+    def test_renew_tokens(self):
+        pass
+
+    def test_update_profile(self):
+        pass
+
+    def test_get_user(self):
+        pass
+
+    def test_initiate_change_password(self):
+        pass
+
+    def test_send_verification(self):
+        pass
+
+    def test_validate_verification(self):
+        pass
+
+    def test_initiate_forgot_password(self):
+        pass
+
+    def test_confirm_forgot_password(self):
+        pass
+
+    def test_change_password(self):
+        pass
+
+    def test_set_attributes(self):
         pass
 
