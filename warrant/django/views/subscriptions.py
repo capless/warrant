ec0e55f14660e64bf0c99c1edcc55a483dbe9a4f
Brian Jinwright <bjinwright@qwigo.com>
2017-03-27 00:11:17 -0400

    Added most of the functionality of the subscription views

--- warrant/django/views/subscriptions.py
+++ warrant/django/views/subscriptions.py
@@ -1,20 +1,16 @@
 import boto3
+from django.contrib.auth import get_user_model
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils.module_loading import import_string
 from django.views.generic import FormView
-from django.views.generic.list import MultipleObjectMixin
+from django.views.generic.list import MultipleObjectMixin, ListView
 
 from django.conf import settings
 from warrant import UserObj
 
-class MySubsriptions(LoginRequiredMixin,MultipleObjectMixin,FormView):
-    template_name = 'warrant/account/subscriptions.html'
-    client = boto3.client('apigateway')
 
-    def get_form_class(self):
-        return import_string(settings.WARRANT_SUBSCRIPTION_FORM)
-
+class GetCognitoUserMixin(object):
 
     def get_user_object(self):
         cog_client = boto3.client('cognito-idp')
@@ -25,17 +21,30 @@ class MySubsriptions(LoginRequiredMixin,MultipleObjectMixin,FormView):
 
     def get_queryset(self):
         u = self.get_user_object()
-
         my_plans = self.client.get_usage_plans(keyId=u.api_key_id)
         return my_plans.get('items',[])
 
 
+class MySubsriptions(LoginRequiredMixin,GetCognitoUserMixin,ListView):
+    template_name = 'warrant/account/subscriptions.html'
+    client = boto3.client('apigateway')
 
-class AdminSubscriptions(UserPassesTestMixin,MySubsriptions):
+
+class AdminSubscriptions(UserPassesTestMixin,GetCognitoUserMixin,
+                         MultipleObjectMixin,FormView):
+    template_name = 'warrant/account/admin-subscriptions.html'
+
 
     def test_func(self):
-        pass
+        return self.request.user.has_perm('can_edit')
 
+    def get_form_class(self):
+        return import_string(settings.WARRANT_SUBSCRIPTION_FORM)
 
+    def get_context_data(self, **kwargs):
+        kwargs['object_list'] = self.object_list = self.get_queryset()
+        context = super(AdminSubscriptions, self).get_context_data(**kwargs)
+        return context
 
-
+    def form_valid(self, form):
+        super(AdminSubscriptions, self).form_valid(form)
