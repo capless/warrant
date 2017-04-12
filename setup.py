6e5ae7c5c6f3f948216a50a962a5a43bd18a5959
Brian Jinwright <bjinwright@qwigo.com>
2017-04-12 12:29:51 -0400

    Updated setup.py and added .bumpversion.cfg

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 import os
 
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+install_reqs = parse_requirements('requirements.txt', session=False)
 
-packages = find_packages(exclude=['tests*', 'tests', 'warrant/tests*', 'cdu*', 'cdu'])
+version = '0.1.0'
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 setup(
     name='warrant',
-    version='0.1',
-    packages=packages,
-    include_package_data=True,
-    description='Cognito integration using boto3.',
+    version=version,
+    description="""Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support.""",
     long_description=README,
-    author='MetaMetrics, Inc.',
-    author_email='brian@ipoots.com',
-    url='http://www.metametricsinc.com/',
-    license='GPLv3',
-    install_requires=[
-        'boto3>=1.4,<1.5','PyJWT==1.4.2','envs>=0.3.0'
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
     ],
-    extras_require={
-        'django': [
-            'Django>=1.8,<1.11',
-            'mock==2.0.0'
-        ],
-    }
+    keywords='aws,cognito,api,gateway,django,capless',
+    author='MetaMetrics Inc and Capless.io',
+    author_email='opensource@capless.io',
+    maintainer='Brian Jinwright',
+    packages=find_packages(),
+    url='https://github.com/capless/warrant',
+    license='GNU GPL V3',
+    install_requires=[str(ir.req) for ir in install_reqs],
+    include_package_data=True,
+    zip_safe=False,
 )
