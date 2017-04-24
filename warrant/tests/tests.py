30c3947a6cd08fe5df8ffb56b3889680e5167ec9
Brian Jinwright <bjinwright@qwigo.com>
2017-04-24 11:05:43 -0400

    Added whitespace below the last test class

--- warrant/tests/tests.py
+++ warrant/tests/tests.py
@@ -1,6 +1,5 @@
 import unittest
 
-from botocore.exceptions import ClientError
 from mock import patch
 from envs import env
 
@@ -168,6 +167,7 @@ class AWSSRPTestCase(unittest.TestCase):
         self.assertTrue('IdToken' in tokens['AuthenticationResult'])
         self.assertTrue('AccessToken' in tokens['AuthenticationResult'])
         self.assertTrue('RefreshToken' in tokens['AuthenticationResult'])
+        
 
 if __name__ == '__main__':
     unittest.main()
