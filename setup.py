71a47855abf01b33d5996409086f40a5ca88feae
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-09-06 19:44:20 -0700

    Consolidate test requirements in file

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
+test_reqs = parse_requirements('requirements_test.txt', session=False)
 
 version = '0.2.0'
 
@@ -26,8 +27,11 @@ setup(
     maintainer='Brian Jinwright',
     packages=find_packages(),
     url='https://github.com/capless/warrant',
-    license='GNU GPL V3',
+    license='Apache License 2.0',
     install_requires=[str(ir.req) for ir in install_reqs],
+    extras_require={
+        'test': [str(ir.req) for ir in test_reqs]
+    },
     include_package_data=True,
     zip_safe=True,
 
