02ed1baef57f7b85183e4dd61eb7c44fb1614da0
Marko Teiste <mte@iki.fi>
2018-09-03 08:30:18 +0300

    Fix setup.py and requirements
    Do not require specific backend for python-jose (see final comment
    on https://github.com/mpdavis/python-jose/issues/79).

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-import os
-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-test_reqs = parse_requirements('requirements_test.txt', session=False)
+install_reqs = [
+    "boto3>=1.4.3",
+    "envs>=0.3.0",
+    "python-jose>=3.0.0",
+    "requests>=2.13.0"
+]
 
-version = '0.6.1'
+test_reqs = [
+    "mock>=2.0.0",
+    "nose",
+    "coverage"
+]
+
+version = '0.6.2'
 
 README="""Python class to integrate Boto3's Cognito client so it is easy to login users. With SRP support."""
 
@@ -28,9 +35,9 @@ setup(
     packages=find_packages(),
     url='https://github.com/capless/warrant',
     license='Apache License 2.0',
-    install_requires=[str(ir.req) for ir in install_reqs],
+    install_requires=install_reqs,
     extras_require={
-        'test': [str(ir.req) for ir in test_reqs]
+        'test': test_reqs
     },
     include_package_data=True,
     zip_safe=True,
