d4bf0c37738a53e7aefda88609d558571dafadf0
Michael Misshore <michael@mudbugmedia.com>
2020-01-03 15:17:44 -0600

    Configure aws_srp methods to accept user context data.
    Likewise, allow for client overrides

--- warrant/aws_srp.py
+++ warrant/aws_srp.py
@@ -197,13 +197,14 @@ class AWSSRP(object):
                 self.get_secret_hash(self.username, self.client_id, self.client_secret)})
         return response
 
-    def authenticate_user(self, client=None):
+    def authenticate_user(self, client=None, user_context_data=None):
         boto_client = self.client or client
         auth_params = self.get_auth_params()
         response = boto_client.initiate_auth(
             AuthFlow='USER_SRP_AUTH',
             AuthParameters=auth_params,
-            ClientId=self.client_id
+            ClientId=(client or self.client_id),
+            UserContextData=user_context_data
         )
         if response['ChallengeName'] == self.PASSWORD_VERIFIER_CHALLENGE:
             challenge_response = self.process_challenge(response['ChallengeParameters'])
@@ -219,13 +220,14 @@ class AWSSRP(object):
         else:
             raise NotImplementedError('The %s challenge is not supported' % response['ChallengeName'])
 
-    def set_new_password_challenge(self, new_password, client=None):
+    def set_new_password_challenge(self, new_password, client=None, user_context_data=None):
         boto_client = self.client or client
         auth_params = self.get_auth_params()
         response = boto_client.initiate_auth(
             AuthFlow='USER_SRP_AUTH',
             AuthParameters=auth_params,
-            ClientId=self.client_id
+            ClientId=(client or self.client_id),
+            UserContextData=user_context_data
         )
         if response['ChallengeName'] == self.PASSWORD_VERIFIER_CHALLENGE:
             challenge_response = self.process_challenge(response['ChallengeParameters'])
