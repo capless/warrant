2792a7358e665036d831d3c693fc2f118492b0fa
Michael Misshore <michael@mudbugmedia.com>
2020-01-07 07:17:27 -0600

    Update pip.req import syntax

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 test_reqs = parse_requirements('requirements_test.txt', session=False)
