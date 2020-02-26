f5943caef90c501d48e5b3ca8c91bd3220458533
Pascal Vizeli <pascal.vizeli@syshack.ch>
2020-02-26 15:09:45 +0100

    Fix setup install for py37 (#3)
    Co-authored-by: kritchie <ritchie.karl@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 test_reqs = parse_requirements('requirements_test.txt', session=False)
